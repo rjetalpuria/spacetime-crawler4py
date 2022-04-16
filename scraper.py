11f03fbf894d9fd9665251a25d7a3e42223b2017
Rushi Jetalpuria <rushi.jetalpuria@gmail.com>
2022-04-16 18:00:34 +0000

    crawls based on resp.status

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,15 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    links = list()
+    print('url: ', url)
+    print('resp.url: ', resp.url)
+    if(resp.status >= 200 or resp.status < 300) and (resp.raw_response is not None):
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        for link in soup.find_all('a'):
+            links.append(link.get('href'))
+    
+    return links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
