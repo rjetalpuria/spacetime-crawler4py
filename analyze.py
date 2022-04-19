87e205488598fde092a35bb3bdea86a121283fce
Rushi Jetalpuria <rushi.jetalpuria@gmail.com>
2022-04-19 23:31:33 +0000

    added stopword filtering

--- analyze.py
+++ analyze.py
@@ -1,3 +1,5 @@
+stopwords = { "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" }
+
 def validate(word): # O(n) where n is the length of the word
     valid_last_chars = ['.', ',', ';', ':', '?', '!', '\"', '\'']
     begin_punct = False
@@ -36,8 +38,10 @@ def validate(word): # O(n) where n is the length of the word
         word = word[:-1] # remove 1 char from the back
     if begin_punct:
         word = word[1:] # remove 1 char from the front
-
-    return word;
+    word = word.lower()
+    if word in stopwords:
+        return False
+    return word
 
 
 # Read the file line-by-line and process each word and return a list of valid alphanumeric 
@@ -48,7 +52,7 @@ def tokenize(file): # O(w * n) = O(w) where w is the number of words in the file
         for word in line.split(): # process each line word-by-word
             result = validate(word) # validate each word
             if result != False: # if the word is valid
-                words.append(result.lower()) #append word to the list
+                words.append(result) #append word to the list
     return words
 
 # returns a dictionary filled with words as key and their frequency as their value
