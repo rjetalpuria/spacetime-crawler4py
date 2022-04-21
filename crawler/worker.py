7fc39dc977c6d1b5c9df3fe9b3c360efce5b4052
Rushi Jetalpuria <rushi.jetalpuria@gmail.com>
2022-04-21 23:02:43 +0000

    added politeness delay in worker:

--- crawler/worker.py
+++ crawler/worker.py
@@ -6,8 +6,12 @@ from utils import get_logger
 import scraper
 import time
 
+from urllib.parse import urlparse
+
+last_queried = dict() #dictionary with hashed domain and the last time it was queried
 
 class Worker(Thread):
+    
     def __init__(self, worker_id, config, frontier):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
@@ -22,6 +26,10 @@ class Worker(Thread):
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
+            domain = urlparse(tbd_url).netloc
+            if domain in last_queried and last_queried[domain] - time.time() < 1: # if it has been less than 1 second from the last time we queried this domain
+                time.sleep(1) #sleep for 1 second regardless of the amount of time
+            last_queried[domain] = time.time() #update last_queried for this domain
             resp = download(tbd_url, self.config, self.logger)
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
